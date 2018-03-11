157367a2b53deb0c87da9657fc446557fb8c41a5
Nirmal Kumar Sekar <kagosekar@gmail.com>
2018-03-11 22:20:56 +0530

    Added form for battle

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material';
 import {MatCardModule} from '@angular/material/card';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatInputModule} from '@angular/material/input';
+
 @NgModule({
-  imports: [MatToolbarModule , MatCardModule , FlexLayoutModule],
-  exports: [MatToolbarModule , MatCardModule , FlexLayoutModule]
+  imports: [MatToolbarModule , MatCardModule , FlexLayoutModule , MatInputModule ],
+  exports: [MatToolbarModule , MatCardModule , FlexLayoutModule , MatInputModule ]
 })
 export class MaterialModule {}
